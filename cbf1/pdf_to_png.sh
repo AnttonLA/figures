c218e13f112dd962fa93bdcf0fcf628477c18133
AnttonLA <anttonlamarca@gmail.com>
2023-09-11 17:22:45 +0200

    Finalize alt layout. Add second .tex script celltypes_alt_arrows.tex that adds arrows on top of the first pdf.

--- cbf1/pdf_to_png.sh
+++ cbf1/pdf_to_png.sh
@@ -2,4 +2,4 @@
 
 # Script to convert pdf to png using ImageMagick
 
-convert -density 300 celltypes_alt.pdf[0] celltypes_alt.png
+convert -density 300 celltypes_alt_arrows.pdf[0] celltypes_alt_arrows.png
